2fb4a7a7a55779828046f2c7dbea5c3231021fe5
JeanRoses <jeanpaulpurihuamandelacruz2019@gmail.com>
2026-01-13 23:33:00 -0500

    Add tremolo effect
    Classic amplitude modulation of the input signal, unlike the am effect which generates its own signal. This actually modulates the incoming audio like old Fender amps would call vibrato (even though it is really amplitude modulation, not frequency modulation - but that is a classic Fender misnomer).
    
    Uses the shared effect_lfo and effect_depth infrastructure, making it trivially simple. The pot1 controls the LFO rate with the usual quadratic mapping to make the lower frequencies more controllable (0.5-10.5 Hz range), pot2 controls the modulation depth.

--- convert.c
+++ convert.c
@@ -23,6 +23,7 @@ typedef unsigned int uint;
 #include "am.h"
 #include "phaser.h"
 #include "discont.h"
+#include "tremolo.h"
 
 struct {
 	float attack, decay, value;
@@ -49,7 +50,7 @@ struct effect {
 	float (*step)(float);
 } effects[] = {
 	EFF(discont), EFF(phaser), EFF(flanger), EFF(echo),
-	EFF(fm), EFF(am),
+	EFF(fm), EFF(am), EFF(tremolo),
 	EFF(magnitude),
 };
 
